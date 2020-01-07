14eec80a68c1b70a3c4f6fd2701b57c83510b667
Chris Ashman <chrisashman96@gmail.com>
2020-01-07 01:17:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -22,7 +22,7 @@ def introduction_with_language_optional(name, language = "Ruby")
 end
 
 introduction_with_language_optional("Edwin", "Python")
-
+introduction_with_language_optional("Edwin")
 
 
 
