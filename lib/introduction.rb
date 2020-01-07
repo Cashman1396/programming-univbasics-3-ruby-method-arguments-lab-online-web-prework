c1b43b978b110ff0f258fc461bd7b3512a427e7f
Chris Ashman <chrisashman96@gmail.com>
2020-01-07 01:16:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,12 +8,14 @@ def introduction(name)
 end
 
 introduction("Josh")
+introduction("Dan")
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 introduction_with_language("Josh", "React")
+introduction_with_language("Dan", "Ember.js")
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n "
